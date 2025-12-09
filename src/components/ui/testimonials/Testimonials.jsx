294dd18fa4e5919dcd88f1616801ff40c7b94d89
G-Chandu-Balaji <balaji5220771@gmail.com>
2025-12-09 17:20:23 +0530

    moved mockData-reviews to separate file

--- src/components/ui/testimonials/Testimonials.jsx
+++ src/components/ui/testimonials/Testimonials.jsx
@@ -1,55 +1,6 @@
 import React, { Fragment } from "react";
 import TestimonialsCard from "./TestimonialsCard";
-const Reviews = [
-  {
-    description:
-      "I never thought online therapy could feel this personal. The therapist listened without judging and helped me break down problems I carried for years. Small exercises they gave me actually worked, and my anxiety started reducing. I feel mentally stronger now, and the best part is that I can reach out anytime I need support.",
-    stars: 5,
-    name: "Tushar Sharma",
-    designation: "CEO, Tushar Groups Pvt. Ltd",
-    imageURL: "./Avatar.png",
-  },
-  {
-    description:
-      "I never thought online therapy could feel this personal. The therapist listened without judging and helped me break down problems I carried for years. Small exercises they gave me actually worked, and my anxiety started reducing. I feel mentally stronger now, and the best part is that I can reach out anytime I need support.",
-    stars: 5,
-    name: "Tushar Sharma",
-    designation: "CEO, Tushar Groups Pvt. Ltd",
-    imageURL: "./Avatar.png",
-  },
-  {
-    description:
-      " I never thought online therapy could feel this personal. The therapist listened without judging and helped me break down problems I carried for years. Small exercises they gave me actually worked, and my anxiety started reducing. I feel mentally stronger now, and the best part is that I can reach out anytime I need support.",
-    stars: 5,
-    name: "Tushar Sharma",
-    designation: "CEO, Tushar Groups Pvt. Ltd",
-    imageURL: "./Avatar.png",
-  },
-  {
-    description:
-      " I never thought online therapy could feel this personal. The therapist listened without judging and  helped me break down problems I carried for years. Small exercises they gave me actually worked, and my anxiety started reducing. I feel mentally stronger now, and the best part is that I can reach out anytime I need support.",
-    stars: 5,
-    name: "Tushar Sharma",
-    designation: "CEO, Tushar Groups Pvt. Ltd",
-    imageURL: "./Avatar.png",
-  },
-  {
-    description:
-      " I never thought online therapy could feel this personal. The therapist listened without judging and  helped me break down problems I carried for years. Small exercises they gave me actually worked, and my anxiety started reducing. I feel mentally stronger now, and the best part is that I can reach out anytime I need support.",
-    stars: 5,
-    name: "Tushar Sharma",
-    designation: "CEO, Tushar Groups Pvt. Ltd",
-    imageURL: "./Avatar.png",
-  },
-  {
-    description:
-      " I never thought online therapy could feel this personal. The therapist listened without judging and  helped me break down problems I carried for years. Small exercises they gave me actually worked, and my anxiety started reducing. I feel mentally stronger now, and the best part is that I can reach out anytime I need support.",
-    stars: 5,
-    name: "Tushar Sharma",
-    designation: "CEO, Tushar Groups Pvt. Ltd",
-    imageURL: "./Avatar.png",
-  },
-];
+import { Reviews } from "../../../data/review";
 
 function Testimonials() {
   return (
